0d9aac96a60a4f9e5a8ae3e01d0555fbd9149ef6
Benson, Derek (IM&T, Pullenvale) <Derek.Benson@csiro.au>
2022-12-09 16:26:06 +1100

    Added explanation for squeue options

--- master_sbatch.sh
+++ master_sbatch.sh
@@ -11,4 +11,5 @@ jid1=$(sbatch --parsable qc_sbatch.sh)
 jid2=$(sbatch --dependency=afterany:$jid1 --parsable align_sbatch.sh)
 jid3=$(sbatch --dependency=afterany:$jid2 --parsable callvariants_sbatch.sh)
 
+# Show job dependencies in queue
 squeue -u $USER -o "%.8A %.4C %.10m %.20E"
