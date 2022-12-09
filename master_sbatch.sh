8b1d8de2809b8c665ee776b24748ad1c15708dfb
Benson, Derek (IM&T, Pullenvale) <Derek.Benson@csiro.au>
2022-12-09 16:24:22 +1100

    Make sbatch produce parsable output

--- master_sbatch.sh
+++ master_sbatch.sh
@@ -4,11 +4,11 @@
 source pipelinefunc.sh;get_ena;download
 
 # For SLURM logs
-mkdir logs
+mkdir -p logs
 
 # Submit jobs with dependencies
-jid1=$(sbatch qc_sbatch.sh)
-jid2=$(sbatch  --dependency=afterany:$jid1 align_sbatch.sh)
-jid3=$(sbatch  --dependency=afterany:$jid2 callvariants_sbatch.sh)
+jid1=$(sbatch --parsable qc_sbatch.sh)
+jid2=$(sbatch --dependency=afterany:$jid1 --parsable align_sbatch.sh)
+jid3=$(sbatch --dependency=afterany:$jid2 --parsable callvariants_sbatch.sh)
 
 squeue -u $USER -o "%.8A %.4C %.10m %.20E"
